ed804fbd0181c461c6ee84043673fc9d465b1d39
HannaHalka <hanna.dm.galka@gmail.com>
2025-03-30 23:45:27 +0300

    Rosenbrock function

--- Lab-3/Rosenbrock_function.py
+++ Lab-3/Rosenbrock_function.py
@@ -15,19 +15,54 @@ def compute_gradient(vector_x):
     return np.array([df_dx, df_dy])
 
 
-def newton():
-    pass
+def compute_hessian(vector_x):
+    x_val, y_val = vector_x[0], vector_x[1]
+    h11, h12 = 120 * x_val**2 - 40 * y_val + 2, -40 * x_val
+    h21 = h12
+    h22 = 20
+    return np.array([[h11, h12], [h21, h22]])
 
 
-tol = 1e-6
-max_iter = 10000
+def newton(vector_x, max_iter=10000, tol=1e-6):
+    x = np.array(vector_x, dtype=float)
+    x_history = [x]
+    f_history = [calculate_objective(x)]
+    grad_history = []
 
-x_00 = np.array([2, 4])
-x_01 = np.array([-2, 10])
+    for i in range(max_iter):
+        grad = compute_gradient(x)
+        hess = compute_hessian(x)
 
-plt.plot(...)
-plt.title("Stochastic Gradient Descent")
-plt.xlabel("Iteration")
-plt.ylabel("Objective values")
-plt.grid(True)
+        d = np.linalg.solve(hess, -grad)
+        x = x + d
+
+        x_history.append(x)
+        f_history.append(calculate_objective(x))
+        grad_history.append(grad)
+
+        if abs(f_history[-1] - f_history[-2]) <= tol:
+            break
+
+    return x_history, grad_history, f_history, i+1
+
+
+x1, x2 = np.array([2, 4]), np.array([-2, 10])
+
+x_hist1, g_hist1, f_hist1, iter1 = newton(x1)
+x_his2, g_hist2, f_hist2, iter2 = newton(x2)
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 5), dpi=100)
+
+ax[0].plot(f_hist1, color="black")
+ax[0].set_title("Newton's Method (start: [2, 4])")
+ax[0].set_xlabel("Iteration")
+ax[0].set_ylabel("Objective value")
+ax[0].grid(True)
+
+ax[1].plot(f_hist2, color="black")
+ax[1].set_title("Newton's Method (start: [-2, 10])")
+ax[1].set_xlabel("Iteration")
+ax[1].set_ylabel("Objective value")
+ax[1].grid(True)
+
 plt.show()
